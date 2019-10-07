42228f2def61a47cae5d8fac3fe465b7f1af5e9f
ARYDURANS <45182113+ARYDURANS@users.noreply.github.com>
2019-10-07 11:33:17 -0300

    Add files via upload

---
+++ Triangulo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+        Scanner d = new Scanner(System.in);
+
+        System.out.println("digite a medida");
+        double a = d.nextDouble();
+        System.out.println(" digite a medida");
+        double b = d.nextDouble();
+        System.out.println("digite  a medida");
+        double c = d.nextDouble();
+
+        if (a == b && b == c) {
+            System.out.println(" triangulo equilatero");
+        }else {
+
+            if (a == b && b !=c ){
+                System.out.println(" triangulo isoceles");
+
+            }else if (a != b && b != c){
+              System.out.println(" e escaleno");
+
+
+                }
+            }
+            }
+        }
