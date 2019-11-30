328c7c64d4c85366b4f1f16077ffb8dcde6a9c98
ARYDURANS <45182113+ARYDURANS@users.noreply.github.com>
2019-11-30 07:38:36 -0300

    Add files via upload

---
+++ setima lista questao 3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+    public class TesteNum{
+
+        public static void main(String args[]){
+
+            String saida="";
+
+            int i = Integer.parseInt(entrada);
+
+            if(i % 2 == 0){
+                saida+=“O numero é par”;
+            }else{
+                saida+=“O numero é impar”;
+            }
+
+
+            System.exit(0);
+
+        }
+    }
